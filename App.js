bebd5aa49bbf61eeb3d1fe5003cd12ac1cc40c3c
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 07:55:37 +0100

    --- Auto-Git Commit ---

--- App.js
+++ App.js
@@ -7,14 +7,10 @@ import {loadUser} from "./redux/actions/auth.js"
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
-
  function App() {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator> */}
+ 
     </NavigationContainer>
   );
 }
