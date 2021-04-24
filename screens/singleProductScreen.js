bce70093e0923a90a89255882f4e6c9aac1c17a9
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 14:14:14 +0100

    --- Auto-Git Commit ---

--- screens/singleProductScreen.js
+++ screens/singleProductScreen.js
@@ -21,8 +21,8 @@ export default class Product extends Component {
             }} />
 
             <View style = {styles.details}>
-                <Text>{item.name}</Text>
-                <Text>{item.price}</Text>
+                <Text style = {styles.text}>{item.name}</Text>
+                <Text style = {styles.text}>{item.price}</Text>
 
             </View>
 
@@ -39,5 +39,8 @@ const styles =StyleSheet.create({
     image : {
         height : "60%",
         width : "100%"
+    },
+    text : {
+        fontSize : 30
     }
 })
