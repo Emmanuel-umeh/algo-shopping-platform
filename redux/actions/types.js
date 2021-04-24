00a613960c8e299dd603ac34573eb664e107fe6b
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 14:32:17 +0100

    --- Auto-Git Commit ---

--- redux/actions/types.js
+++ redux/actions/types.js
@@ -5,3 +5,10 @@ export const AUTH_ERROR = "AUTH_ERROR" // auth error loaded action type
 export const LOGIN_SUCCESS =  'LOGIN_SUCCESS' //login success action type
 export const LOGIN_FAIL =  'LOGIN_FAIL' //login failed action type
 export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS" // logs out the user
+
+
+// order
+
+export const ORDER_FAILED =  'ORDER_FAILED' //order failed
+export const ORDER_SUCCESSFUL = "ORDER_SUCCESSFUL" // order was succesfsul
+
