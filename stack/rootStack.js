9b4c685eceb7480339e22b8502dac3af9fb2ef41
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 13:22:39 +0100

    --- Auto-Git Commit ---

--- stack/rootStack.js
+++ stack/rootStack.js
@@ -1,41 +1,39 @@
+import React from "react";
 
-
-
-import React from 'react';
-
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from '../screens/loginScreen';
-import Shop from '../screens/shopScreen';
-import Product from '../screens/singleProductScreen';
-import { connect } from 'react-redux';
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "../screens/loginScreen";
+import Shop from "../screens/shopScreen";
+import Product from "../screens/singleProductScreen";
+import { connect } from "react-redux";
 
 const Stack = createStackNavigator();
 
-
-
-const RootStack = ({navigation}) => (
-    <Stack.Navigator screenOptions ={{
-        headerStyle :{
-            backgroundColor : "pink"
-        }
-    }}>
-         <Stack.Screen name="Shop" component={Shop} options={{
-
-title : "Latest products",
-headerTitleStyle : {
-    color : "white",
-    alignSelf : "center"
-}
-         }}/>
-         <Stack.Screen name="Product" component={Product}/>
-        
-    </Stack.Navigator>
+const RootStack = ({ navigation }) => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: "crimson",
+      },
+    }}
+  >
+    <Stack.Screen
+      name="Shop"
+      component={Shop}
+      options={{
+        title: "Latest products",
+        headerTitleStyle: {
+          color: "white",
+          alignSelf: "center",
+        },
+      }}
+    />
+    <Stack.Screen name="Product" component={Product} />
+  </Stack.Navigator>
 );
 
 const mapStateToProps = (state) => ({
-    auth: state.auth,
-  });
-  
-  // export default ProjectForm
-  export default connect(mapStateToProps, null)(RootStack);
-  
+  auth: state.auth,
+});
+
+// export default ProjectForm
+export default connect(mapStateToProps, null)(RootStack);
