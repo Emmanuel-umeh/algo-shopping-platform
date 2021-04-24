e1e396f6e4f788e025ec0ec4cb762b234c099004
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 09:56:07 +0100

    --- Auto-Git Commit ---

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TextInput } from 'react-native'
 
 export default class Login extends Component {
     render() {
         return (
-            <View>
+            <View style = {styles.container}>
 
                 <Text>Login Screen</Text>
                 
