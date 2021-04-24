7efbd66aa0afac2d583712503108d4cebc67bf92
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 07:43:34 +0100

    --- Auto-Git Commit ---

--- redux/store.js
+++ redux/store.js
@@ -2,15 +2,23 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware  from 'redux-thunk';
 import rootReducer from './reducers';
 
-const initialState = {};
+import AsyncStorage from '@react-native-community/async-storage';
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistenceConfigs = {
+  key: "order", // whatever you want to keep as your key
+  storage: AsyncStorage
+}
+
+const persistedReducer = persistReducer(persistenceConfigs, rootReducer);
 
 const middleWare = [thunkMiddleware ];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  rootReducer,
-  initialState,
+  persistedReducer,
   composeEnhancers(applyMiddleware(...middleWare))
 );
+export const persistedStore = persistStore(store);
 
 export default store;
