aeed2fd219ffaa166dd177683e867f131194fb74
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 07:55:22 +0100

    --- Auto-Git Commit ---

--- stack/Authstack.js
+++ stack/Authstack.js
@@ -13,10 +13,10 @@ const Authstack = createStackNavigator();
 
 const Authstack = ({navigation}) => (
     <Authstack.Navigator>
-        {/* <RootStack.Screen name="SplashScreen" component={SplashScreen}/> */}
+    
          <Authstack.Screen name="login" component={Login}/>
         
-        {/* <RootStack.Screen   name="mapDestination" component={MapDestination}/> */}
+
 
 
 
