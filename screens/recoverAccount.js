4719a5cebf06f6118fa21e3e4320aa270326ae3b
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 09:57:37 +0100

    --- Auto-Git Commit ---

---
+++ screens/recoverAccount.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, TextInput } from 'react-native'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <View style = {styles.container}>
+
+               
+                 <Text>Recover Screen</Text>
+
+               
+                 <Text>Login Screen</Text>
+            </View>
+        )
+    }
+}
+
+
+const styles =  StyleSheet.create({
+    container : {
+        alignContent : "center",
+        justifyContent : "center"
+    }
+})
