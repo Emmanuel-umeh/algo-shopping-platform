9807c5edce821442cc6399989150cea2dcf216a9
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 16:26:56 +0100

    --- Auto-Git Commit ---

--- redux/actions/order.js
+++ redux/actions/order.js
@@ -1,64 +1,49 @@
-import {
-    ORDER_FAILED,
-    ORDER_SUCCESSFUL
-  } from "./types";
-  
-  import axios from "axios"
-  
+import { ORDER_FAILED, ORDER_SUCCESSFUL } from "./types";
+
+import axios from "axios";
+
 axios.defaults.baseURL = "https://algorandapi.herokuapp.com";
 
+export const purchase = (price, mnemonic) => async (dispatch, getState) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-  export const purchase = (price, mnemonic) => async (dispatch, getState) => {
-    try {
+    const body = JSON.stringify({ price, mnemonic });
 
-        
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
+    const response = await axios.post(`/auth/purchase`, body, config);
 
-      const body = JSON.stringify({ price,mnemonic });
-  
-      
+    // console.log({response})
+    const { success } = response.data;
 
-      
-      const response = await axios.post(
-        `/auth/purchase`,  body,config
-      );
-  
-      // console.log({response})
-      const {success} = response.data
-  
-      if(!success){
-          dispatch({
-              type : ORDER_FAILED
-          })
-  
-          alert("Failed to purchase product")
-          
-          return "Failed to purchase product"
-      }
-      
+    if (!success) {
       dispatch({
-        type: ORDER_SUCCESSFUL,
-
+        type: ORDER_FAILED,
       });
 
-      alert("Product Purchased Successfully!!")
-  
-  
-  
-    } catch (error) {
-      console.log({ error });
+      alert("Failed to purchase product");
 
-      alert(error.response.msg ? error.response.msg : "Something went wrong. Please try again")
-  
-      dispatch({
-          type : ORDER_FAILED
-      })
-      return ""
+      return "Failed to purchase product";
     }
-  };
-  
- 
+
+    dispatch({
+      type: ORDER_SUCCESSFUL,
+    });
+
+    return "Product Purchased Successfully!!";
+  } catch (error) {
+    console.log({ error });
+
+    // alert(error.response.msg ? error.response.msg : "Something went wrong. Please try again")
+
+    dispatch({
+      type: ORDER_FAILED,
+    });
+    return error.response.msg
+      ? error.response.msg
+      : "Something went wrong. Please try again";
+  }
+};
