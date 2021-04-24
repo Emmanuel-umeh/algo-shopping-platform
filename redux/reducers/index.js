02046afd2947a333f1891b1394dda0c40b92af21
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 07:10:20 +0100

    --- Auto-Git Commit ---

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -3,24 +3,6 @@ import { combineReducers } from 'redux';
 
 export default combineReducers({
     // error: errorReducer,
-    auth: authReducer,
-    campaign : campaignReducer,
-    error:errorReducer,
-    profile:profileReducer,
-    organisation:organisationReducer,
-    pay:payReducer,
-    dashboard:dashboardReducer,
-    feedback:feedbackReducer,
-    contributors:contributorReducers,
-    tx:txReducer,
-    admin:adminReducer,
-    flutterwave:flutterwaveReducer,
-    payout:payoutReducer,
-    blog:blogReducer,
-    badge:badgeReducer,
-    newsletter:newsletterReducer,
-    userRed:userReducer,
-    writer:writerReducer
-    // blockchain:blockchainReducer
+    auth: authReducer
   });
   
