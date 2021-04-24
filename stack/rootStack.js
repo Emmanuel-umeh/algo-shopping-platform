80fadb64a718f394f5abd29dd470430c9e5acca7
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 07:50:19 +0100

    --- Auto-Git Commit ---

--- stack/rootStack.js
+++ stack/rootStack.js
@@ -5,6 +5,7 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../screens/loginScreen';
+import Shop from '../screens/shopScreen';
 
 
 const RootStack = createStackNavigator();
@@ -15,6 +16,8 @@ const RootStackScreen = ({navigation}) => (
     <RootStack.Navigator>
         {/* <RootStack.Screen name="SplashScreen" component={SplashScreen}/> */}
          <RootStack.Screen name="shop" component={Login}/>
+         <RootStack.Screen name="shop" component={Shop}/>
+         <RootStack.Screen name="shop" component={}/>
         
         {/* <RootStack.Screen   name="mapDestination" component={MapDestination}/> */}
 
