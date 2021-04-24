a26caf2b24fc05c888ec644a6db8f11826c863b6
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 07:53:52 +0100

    --- Auto-Git Commit ---

--- stack/rootStack.js
+++ stack/rootStack.js
@@ -7,7 +7,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../screens/loginScreen';
 import Shop from '../screens/shopScreen';
 import Product from '../screens/singleProductScreen';
-
+import { connect } from 'react-redux';
 
 const RootStack = createStackNavigator();
 
