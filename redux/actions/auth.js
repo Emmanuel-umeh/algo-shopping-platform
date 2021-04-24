cddd3d774a8f19d7fd363f783d9d8e39af3982c5
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 07:40:33 +0100

    --- Auto-Git Commit ---

--- redux/actions/auth.js
+++ redux/actions/auth.js
@@ -1,55 +1,46 @@
-
 import {
-    USER_LOADING, 
-    USER_LOADED, 
-    AUTH_ERROR,
-    LOGIN_SUCCESS, 
-    LOGIN_FAIL,
-    LOGOUT_SUCCESS
- } from "../action/types"
-
+  USER_LOADING,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+} from "../action/types";
 
 export const loadUser = () => async (dispatch, getState) => {
+  dispatch({ type: USER_LOADING }); // dispatch user loading
+  let userAddress = "0000";
 
-
-    dispatch({ type: USER_LOADING }); // dispatch user loading
-    let userAddress = '0000'
-
-    dispatch({
-       type: USER_LOADED,
-        payload: userAddress,
-        // recentOrders
-      }); 
-
+  dispatch({
+    type: USER_LOADED,
+    payload: userAddress,
+    // recentOrders
+  });
 };
 
 // algosdk.secretKeyToMnemonic
 export const login = () => async (dispatch, getState) => {
-
-
+  try {
     dispatch({ type: USER_LOADING }); // dispatch user loading
 
-    acct = algosdk.generateAccount();
+    let acct = algosdk.generateAccount();
 
-    userAddress = acct.addr;
-console.log({userAddress});
-var mnemonic = algosdk.secretKeyToMnemonic(acct.sk);
+    var userAddress = acct.addr;
+    console.log({ userAddress });
+    var mnemonic = algosdk.secretKeyToMnemonic(acct.sk);
 
-// var recoveredAccount1 = algosdk.mnemonicToSecretKey(account1_mnemonic);
-var isValid = algosdk.isValidAddress(recoveredAccount1.addr);
-console.log("Is this a valid address: " + isValid);
-console.log("Account created. Save off Mnemonic and address");
-
-    let userAddress = '0000'
-    let mnemonic = ""
+    // var recoveredAccount1 = algosdk.mnemonicToSecretKey(account1_mnemonic);
+    // var isValid = algosdk.isValidAddress(acct.addr);
+    // console.log("Is this a valid address: " + isValid);
+    // console.log("Account created. Save off Mnemonic and address");
 
     dispatch({
-       type: USER_LOADED,
-       userAddress: userAddress,
-        mnemonic :mnemonic
-        // recentOrders
-      }); 
-
+      type: USER_LOADED,
+      userAddress: userAddress,
+      mnemonic: mnemonic,
+      // recentOrders
+    });
+  } catch (error) {
+    console.log({ error });
+  }
 };
-
-
