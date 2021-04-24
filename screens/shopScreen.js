5d7da3d5b9ba04f510395292189b90d6bae48298
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 13:14:23 +0100

    --- Auto-Git Commit ---

--- screens/shopScreen.js
+++ screens/shopScreen.js
@@ -10,3 +10,15 @@ export default class Shop extends Component {
         )
     }
 }
+
+
+Shop.navigationOptions = navData => {
+    title = {
+      text:
+        'All Products',
+      style: {
+        color: Colors.textLight,
+        fontSize: 20
+      }
+    }
+}
