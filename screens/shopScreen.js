a075dd6b39092a7855a2a790e71202f39e471a85
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 13:27:53 +0100

    --- Auto-Git Commit ---

--- screens/shopScreen.js
+++ screens/shopScreen.js
@@ -34,13 +34,37 @@ export default class Shop extends Component {
         ]
       }
     }
+    _renderItem({item,index}){
+        return (
+          <View style={{
+              backgroundColor:'floralwhite',
+              borderRadius: 5,
+              height: 250,
+              padding: 50,
+              marginLeft: 25,
+              marginRight: 25, }}>
+            <Text style={{fontSize: 30}}>{item.title}</Text>
+            <Text>{item.text}</Text>
+          </View>
+
+        )
+    }
 
     render() {
         return (
-            <View>
-                <Text> Shop component</Text>
+          <SafeAreaView style={{flex: 1, backgroundColor:'rebeccapurple', paddingTop: 50, }}>
+            <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center', }}>
+                <Carousel
+                  layout={"default"}
+                  ref={ref => this.carousel = ref}
+                  data={this.state.carouselItems}
+                  sliderWidth={300}
+                  itemWidth={300}
+                  renderItem={this._renderItem}
+                  onSnapToItem = { index => this.setState({activeIndex:index}) } />
             </View>
-        )
+          </SafeAreaView>
+        );
     }
 }
 
