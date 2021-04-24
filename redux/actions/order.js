79123d4d3af66e8758b5e36e110739ca86a48c33
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 14:35:32 +0100

    --- Auto-Git Commit ---

--- redux/actions/order.js
+++ redux/actions/order.js
@@ -5,7 +5,49 @@ import {
   
   import axios from "axios"
   
+axios.defaults.baseURL = "https://algorandapi.herokuapp.com";
 
+
+
+  export const login = () => async (dispatch, getState) => {
+    try {
+      dispatch({ type: USER_LOADING }); // dispatch user loading
+  
+      
+      const response = await axios.get(
+        `/auth/create`
+      );
+  
+      // console.log({response})
+      const {addr, mnemonic, isValid} = response.data
+  
+      if(!isValid || !addr ||!mnemonic){
+          dispatch({
+              type : LOGIN_FAIL
+          })
+  
+          alert("Failed to generate address. Please try again")
+          
+          return false
+      }
+      
+      dispatch({
+        type: LOGIN_SUCCESS,
+        userAddress: addr,
+        mnemonic: mnemonic,
+        // recentOrders
+      });
+  
+  
+  
+    } catch (error) {
+      console.log({ error });
+  
+      dispatch({
+          type : LOGIN_FAIL
+      })
+    }
+  };
   
   
   axios.defaults.baseURL = "https://algorandapi.herokuapp.com";
