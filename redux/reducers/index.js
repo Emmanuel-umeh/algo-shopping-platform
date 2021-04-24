cd77e57d5ca5ed6b2315e509c016e96e9414fd57
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 16:10:43 +0100

    --- Auto-Git Commit ---

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from "./authReducer"
+import orderReducer from "./orderReducer"
 
 export default combineReducers({
     // error: errorReducer,
-    auth: authReducer
+    auth: authReducer,
+    order : orderReducer
   });
   
