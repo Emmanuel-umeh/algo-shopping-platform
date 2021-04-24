ac450d09122a659930db7e9fdd244007d1231af0
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 14:08:44 +0100

    --- Auto-Git Commit ---

--- screens/singleProductScreen.js
+++ screens/singleProductScreen.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import { Image, Text, View } from 'react-native'
 
 export default class Product extends Component {
+
+    componentDidMount(){
+        const {item} = this.props.route.params
+        if(!item){
+            this.props.navigation.pop()
+        }  
+    }
     render() {
 
         const {item} = this.props.route.params
