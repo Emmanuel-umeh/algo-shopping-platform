51ef66b9e79fac1b6d9eb331e77a9af083f596f5
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 07:04:56 +0100

    --- Auto-Git Commit ---

--- stack/Authstack.js
+++ stack/Authstack.js
@@ -6,26 +6,17 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 
-const RootStack = createStackNavigator();
+const Authstack = createStackNavigator();
 
 
 
 const RootStackScreen = ({navigation}) => (
-    <RootStack.Navigator   screenOptions={{
+    <Authstack.Navigator   screenOptions={{
         headerShown: false
       }}>
         {/* <RootStack.Screen name="SplashScreen" component={SplashScreen}/> */}
          <RootStack.Screen name="getStarted" component={getStarted}/>
-        <RootStack.Screen name="phoneNumberScreen" component={PhoneNumber}/>
-        <RootStack.Screen name="otp" component={Otp}/>
-        <RootStack.Screen name="nameScreen" component={NameScreen}/>
-        <RootStack.Screen name="TermsAndCondition" component={TermsAndCondition}/>
-        <RootStack.Screen name="SignInScreen" component={SignInScreen}/>
-        <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/> 
-   
-        <RootStack.Screen headerMode ="none" name="Map" component={Map}/>
-        <RootStack.Screen   name="setDestination" component={setDestination}/>
-
+        
         {/* <RootStack.Screen   name="mapDestination" component={MapDestination}/> */}
 
 
