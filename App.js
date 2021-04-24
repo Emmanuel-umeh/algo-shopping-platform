68a1e6bf981dd31b227d9b4d6a34258298dc80b4
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-04-24 07:22:49 +0100

    --- Auto-Git Commit ---

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {loadUser} from "./redux/actions/auth.js"
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-export default function App() {
+ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -16,6 +18,16 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  error: state.error,
+  order: state.order
+});
+
+// export default ProjectForm
+export default connect(mapStateToProps, { loadUser, setUserToken })(App);
 
 const styles = StyleSheet.create({
   container: {
