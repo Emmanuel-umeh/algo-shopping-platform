de6f435153f277f3b38c87d75894c733e77c6a4b
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-05-10 21:50:11 +0100

    --- Auto-Git Commit ---

--- screens/profileScreen.js
+++ screens/profileScreen.js
@@ -1,65 +1,62 @@
-
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import {View, StyleSheet, Text, TextInput} from "react-native"
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { View, StyleSheet, Text, TextInput } from "react-native";
 
 class Profile extends Component {
+  render() {
+    const { mnemonic } = this.props.auth;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Please ensure you copy and store your mnemonic key down
+        </Text>
 
-    render() {
-        const {mnemonic} = this.props.auth
-        return (
-            <View style = {styles.container}>
-                <Text style = {styles.text}>Please ensure you copy and store your mnemonic key down</Text>
-                
-                <View style={styles.inputView} >
-            <TextInput
-      style={styles.inputText}
-      editable={false} 
-      selectTextOnFocus={false}
-      defaultValue = {mnemonic ? mnemonic : null}
-      underlineColorAndroid="transparent"
-      placeholder="Mnemonic Key"
-      placeholderTextColor="grey"
-      numberOfLines={20}
-      multiline={true}
-    />
-            </View>
-            
-            </View>
-        )
-    }
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.inputText}
+            editable={false}
+            selectTextOnFocus={false}
+            defaultValue={mnemonic ? mnemonic : null}
+            underlineColorAndroid="transparent"
+            placeholder="Mnemonic Key"
+            placeholderTextColor="grey"
+            numberOfLines={20}
+            multiline={true}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
-
-    container : {
-        flex : 1,
-        alignContent: "center",
-        justifyContent : "center"
-    },
-    text : {
-        fontSize : 20,
-        fontWeight : 'bold'
-    },
-    inputText:{
-        // height:0,
-        color:"white"
-      },
-      inputView:{
-        width:"80%",
-        backgroundColor:"#465881",
-        borderRadius:25,
-        height:200,
-        marginBottom:20,
-        justifyContent:"center",
-        alignSelf : "center",
-        padding:20
-      },
-})
-
+  container: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  inputText: {
+    // height:0,
+    color: "white",
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#465881",
+    borderRadius: 25,
+    height: 200,
+    marginBottom: 20,
+    justifyContent: "center",
+    alignSelf: "center",
+    padding: 20,
+  },
+});
 
 const mapStateToProps = (state) => ({
-    auth: state.auth,
-  });
-  
-  // export default ProjectForm
-  export default connect(mapStateToProps, null )(Profile);
+  auth: state.auth,
+});
+
+// export default ProjectForm
+export default connect(mapStateToProps, null)(Profile);
