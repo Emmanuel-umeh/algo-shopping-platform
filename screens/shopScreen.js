7fa8b260ae3501f5a485ed39876bb1198596234d
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-05-10 21:40:25 +0100

    --- Auto-Git Commit ---

--- screens/shopScreen.js
+++ screens/shopScreen.js
@@ -176,6 +176,7 @@ const styles = StyleSheet.create({
     backgroundColor:"#fb5b5a",
     borderRadius:25,
     height:30,
+    alignItems:"center",
     alignSelf : "center",
     justifyContent:"center",
     marginTop:10,
