20a16bb0be005306e6a2ebbcdc28fe2a1bcc5713
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-05-10 21:51:41 +0100

    --- Auto-Git Commit ---

--- stack/Authstack.js
+++ stack/Authstack.js
@@ -1,41 +1,40 @@
-
-
-
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from '../screens/loginScreen';
-import Recover from '../screens/recoverAccount'
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "../screens/loginScreen";
+import Recover from "../screens/recoverAccount";
 
 const Stack = createStackNavigator();
 
+const Authstack = ({ navigation }) => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Login"
+      component={Login}
+      options={{
+        headerShown: false,
+      }}
+    />
 
+    <Stack.Screen
+      options={{
+        title: "Recover Account",
+        headerTitleStyle: {
+          color: "white",
+        },
+        // headerRight: () => (
 
-const Authstack = ({navigation}) => (
-    <Stack.Navigator>
-    
-         <Stack.Screen name="Login" component={Login} options ={{
-         headerShown : false,
-         
-        
-         
-         }}/>
-        
-    
-         <Stack.Screen name="Recover" component={Recover} />
-        
-
-
-
-
-    </Stack.Navigator>
+        // ),
+      }}
+      name="Recover"
+      component={Recover}
+    />
+  </Stack.Navigator>
 );
 
-
 const mapStateToProps = (state) => ({
-    auth: state.auth,
-  });
-  
-  // export default ProjectForm
-  export default connect(mapStateToProps, null)(Authstack);
-  
+  auth: state.auth,
+});
+
+// export default ProjectForm
+export default connect(mapStateToProps, null)(Authstack);
