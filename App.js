59db5552de472fad3ccfe4a72d110a0a9ee84732
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-05-21 01:42:43 +0100

    --- Auto-Git Commit ---

--- App.js
+++ App.js
@@ -1,45 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
 import { loadUser } from "./redux/actions/auth.js";
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Authstack from "./stack/Authstack";
 import RootStack from "./stack/RootStack";
 import { connect } from "react-redux";
-
-import { Provider } from "react-redux";
-
-import { PersistGate } from "redux-persist/integration/react"
-import store, { persistedStore } from "./redux/store";
 function App(props) {
-
-  console.log(props.auth)
   return (
-  
     <NavigationContainer>
-      {props.auth.userAddress ? <RootStack /> :  <Authstack /> }
+      {props.auth.userAddress ? <RootStack /> : <Authstack />}
     </NavigationContainer>
-
-
   );
 }
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
-
 });
 
 // export default ProjectForm
 export default connect(mapStateToProps, { loadUser })(App);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-import { AppLoading, registerRootComponent } from "expo";
+import {registerRootComponent } from "expo";
 import React, { Component } from "react";
 import App from "./App";
 import { Provider } from "react-redux";
-
-import { PersistGate } from "redux-persist/integration/react"
+import { PersistGate } from "redux-persist/integration/react";
 import store, { persistedStore } from "./redux/store";
-
-// import * as firebase from 'firebase';
-
-
-
-
-
-
 class Index extends Component {
   render() {
     return (
       <Provider store={store}>
-        <PersistGate loading={
-        // <AppLoading />
-        null
-        // console.log
-        } persistor={persistedStore}>
-
-        <App />
-        
-
-      
+        <PersistGate
+          loading={
+            null
+          }
+          persistor={persistedStore}
+        >
+          <App />
         </PersistGate>
       </Provider>
     );
