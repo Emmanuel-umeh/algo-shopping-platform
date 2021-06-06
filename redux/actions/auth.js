c9cdafabdfa8e9a286d59f8a88bdc00e3f509045
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-06-06 11:20:06 +0100

    --- Auto-Git Commit ---

--- redux/actions/auth.js
+++ redux/actions/auth.js
@@ -119,3 +119,44 @@ export const recover = (mnemonic) => async (dispatch, getState) => {
       });
   
   };
+
+
+  export const fetch_balance = () => async (dispatch, getState) => {
+    try {
+      dispatch({ type: USER_LOADING }); // dispatch user loading
+  
+      
+      const response = await axios.get(
+        `/auth/fetch-balance`
+      );
+  
+      // console.log({response})
+      const {addr, mnemonic, isValid} = response.data
+  
+      if(!isValid || !addr ||!mnemonic){
+          dispatch({
+              type : LOGIN_FAIL
+          })
+  
+          alert("Failed to generate address. Please try again")
+          
+          return false
+      }
+      
+      dispatch({
+        type: LOGIN_SUCCESS,
+        userAddress: addr,
+        mnemonic: mnemonic,
+        // recentOrders
+      });
+  
+  
+  
+    } catch (error) {
+      console.log({ error });
+  
+      dispatch({
+          type : LOGIN_FAIL
+      })
+    }
+  };
